0e1d487edf8adaf8802a6e88e65ce55aea8ddf29
Mark DiFranco <mpdifran@gmail.com>
2016-01-18 19:59:19 -0500

    Initial Commit

---
+++ MultipeerFileShare/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MultipeerFileShare
+//
+//  Created by Mark DiFranco on 2016-01-18.
+//  Copyright © 2016 Test. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
